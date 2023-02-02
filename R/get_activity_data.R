e89e85e3eb3a9980e1e5a265cc689936dc2e3c23
Tim Cross <tim_cross@me.com>
2023-02-02 16:44:34 +0000

    *(get_activity_data) - additional processing added

--- R/get_activity_data.R
+++ R/get_activity_data.R
@@ -1,11 +1,12 @@
 #' Retrieve activity summary data from Strava API
 #'
-#' \code{get_activity_data} returns an dataframe of activity data from the Strava API.
+#' \code{get_activity_data} returns a dataframe of activity data from the Strava API. The 200 most recent activities are returned by default.
 #'
 #' @param strava_token Strava authentication token.
 #' @param N_to_return Number of activities to return. Defaults to \code{200}.
+#' @param activities_to_exclude Any activity IDs to exclude. Defaults to \code{NA_character}.
 #' @return A dataframe of stream data
-#' @import httr jsonlite
+#' @import httr jsonlite dplyr purrr stringr
 #' @export
 #' @examples
 #' my_activity_df <- get_activity_data(strava_token = my_auth_token,
@@ -13,7 +14,8 @@
 #'
 
 get_activity_data <- function(strava_token,
-                             N_to_return = 200) {
+                             N_to_return = 200,
+                             activities_to_exclude = NA_integer_) {
   
   activity_data <- httr::GET(url = "https://www.strava.com/api/v3/athlete/activities",
                              config = strava_token, 
@@ -22,7 +24,17 @@ get_activity_data <- function(strava_token,
   # Find new activities and process
   activities <- activity_data$content %>%
     rawToChar() %>%
-    jsonlite::fromJSON()
+    jsonlite::fromJSON() %>% 
+    dplyr::filter(!id %in% activities_to_exclude) %>% 
+    dplyr::mutate(start_lat = purrr::map_dbl(start_latlng, 1),
+                  start_lng = purrr::map_dbl(start_latlng, 2),
+                  end_lat = purrr::map_dbl(end_latlng, 1),
+                  end_lng = purrr::map_dbl(end_latlng, 2),
+                  strava_link = stringr::str_glue("https://www.strava.com/activities/{id}"),
+                  ride_start = stringr::str_replace_all(start_date_local, "T|Z", " ") %>% as.POSIXct(),
+                  ride_start = format(ride_start, "%Y-%m-%d %H:%M:%S")) %>% 
+    dplyr::select(-c(athlete, map, start_latlng, end_latlng)) %>% 
+    as_tibble()
   
   return(activities)
   
